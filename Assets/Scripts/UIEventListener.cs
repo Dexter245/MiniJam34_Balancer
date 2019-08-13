4dd5fef03f578fab2642e8a316b1997485363ccb
Lajos Ambrus <Dexter245@googlemail.com>
2019-08-13 11:18:54 +0200

    retry button works

--- Assets/Scripts/UIEventListener.cs
+++ Assets/Scripts/UIEventListener.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIEventListener : MonoBehaviour
 {
@@ -23,7 +24,8 @@ public class UIEventListener : MonoBehaviour
 
     public void OnRetryButtonClick()
     {
-        Debug.Log("retry click");
+        Globals.gameState = GameState.INIT;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
 }
